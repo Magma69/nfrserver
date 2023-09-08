d2663090ab8602a3a252d76de6c05cb9cd297944
Magma69 <rrdoyle41@gmail.com>
2023-09-08 17:34:03 -0400

    put in a test return

---
+++ index.js
@@ -0,0 +1,12 @@
+return {
+    nextTrainning: '0:00',
+    host: '[DM] doylerr411',
+    type: {
+        minRank: 'Supervisor',
+        maxRank: 'Manager'
+    },
+    trainTo: 'Host Supervisor',
+    trainFrom: 'Supervisor',
+    isFull: false
+}
+//This is an excample payload for the training system!
