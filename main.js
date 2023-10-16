6b7a5aceb75394b0a6c053ccc97c599dffc3c86c
magma69 <magmadev8@gmail.com>
2023-10-16 08:57:08 -0400

    fewgrw

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ window.addEventListener("load", async () => {
         "hello": "hi!"
     }
 
-    let request = await fetch(`https://nfrserver.vercel.app/api/`, {
+    let request = await fetch(`https://nfrserver.vercel.app/api/index.js`, {
             method: 'POST',
             body: JSON.stringify(body)
         });
